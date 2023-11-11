30bb327651d1b3eb66d78027e84bbc0ee2ceb8a2
bikramia <bikram.rhce@gmail.com>
2023-11-10 23:28:29 -0500

    wk1d2

---
+++ Learn_Block_Letters_Project/block_letters.py
@@ -0,0 +1,29 @@
+# # Block Letters
+
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+# Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+# ASCII art of letters. For example, the ASCII art of the letter F is comprised of 5 characters in the first row, one character in the second and third rows, three characters in the fourth row, and one character in the fifth, sixth, and seventh row. 
+# These characters when viewed together look like the letter F
+
+# What we are building in this project:
+
+# Take a look at the complete alphabet and find your initials. Notice how each block letter is 7x5 and formed by the letter itself.
+# My initials are S and L, so my initials.py program should output:
+
+#  SSS   L
+# S   S  L
+# S      L
+#  SSS   L
+#     S  L
+# S   S  L
+#  SSS   LLLLL
+
+# Once you are ready, mark this task complete by checking off the box.
+
+print( "SSS   L")
+print("S   S  L")
+print("S      L")
+print( "SSS   L")
+print("    S  L")
+print("S   S  L")
+print(" SSS   LLLLL")
